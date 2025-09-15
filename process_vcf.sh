e2f1d14cca996e1599e677f6b99c929c6920aeb3
Umichyingzhen <yingzhen19970316@gmail.com>
2025-09-15 11:08:44 -0400

    Updated process_vcf.sh on chromosome 10

--- process_vcf.sh
+++ process_vcf.sh
@@ -1,2 +1,2 @@
-grep -v "^#" TNBC_sample.vcf | awk '$1==1 && $6==24' | cut -f1,2,6
-grep -v "^#" TNBC_sample.vcf | awk '$1==1 && $6==24' | cut -f1,2,6 |wc -l
+grep -v "^#" TNBC_sample.vcf | awk '$4=="T" && ($5=="C" || $5=="G")' | cut -f1,2,4-6| wc -l
+grep -v "^#" TNBC_sample.vcf | awk '$1==10' | cut -f1,4,5 | sort | uniq -c | sort -rn | head -n3
